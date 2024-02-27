a9cd8e1ccc5ad11ed4f2edfdb1165890e4b69d35
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-02-27 16:11:30 -0600

    Se agraga libreria de dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //Aqui se va a crear la aplicacion
 //Se van a usar modulos de import y export, para eso se agrego del modulo en el package.json
 import express from "express"
+//Se agrega libreria dotenv que sirve para cargar variables del archivo .env 
+import dotenv from "dotenv"
 const app = express()
-
-//Vamos a crear la conexion con el puerto de la applicacion 
+dotenv.config()
+//Vamos a crear la conexion con el puerto de la applicacion, se corre "npm start" en terminal o bash
 app.listen(8800,()=>{
     console.log("Conexion al backend")
 })
