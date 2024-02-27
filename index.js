0fae33f9205eca53631f5d19910cece6f670e122
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-02-27 16:29:31 -0600

    Se hizo conexion a mongo mediante mongoose

--- index.js
+++ index.js
@@ -3,9 +3,24 @@
 import express from "express"
 //Se agrega libreria dotenv que sirve para cargar variables del archivo .env 
 import dotenv from "dotenv"
+//Se agrega libreria de mongoose para trabajar la base de datos. 
+import mongoose from "mongoose"
+
+//Se va a realizar la conexion de moongose tal como lo indica la pagina oficial de Mongoose
+const connect = async () => { //Esta es la funcion que se llamara cuando se conecte la app a la BD. 
+  try{
+    await mongoose.connect(process.env.MONGO); //se pasa como parametro la constante donde se coloco el link de connexion de Mongodb
+    console.log("Conectado a mongodb") //Este es el mensaje de conexion a la base de datos/.
+  }catch (error){
+    throw error; //Se modifica para que en caso de haber un error la conexion se caiga 
+  }
+};
+
+
 const app = express()
 dotenv.config()
 //Vamos a crear la conexion con el puerto de la applicacion, se corre "npm start" en terminal o bash
 app.listen(8800,()=>{
-    console.log("Conexion al backend")
+    connect() //aqui se llama la funcion creada en la linea 10-16.
+    console.log("Conectado al backend")
 })
