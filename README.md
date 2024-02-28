be238d49ba21d63e7b92257c171b6253d40bb568
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-02-28 13:38:21 -0600

    Se hacen las rutas de API y un endpoint de prueba

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ MongoDB user: Camilo
 password: Ulacit123
 
 Mongo connection string: mongodb+srv://Camilo:<password>@bookcr.xgztqr5.mongodb.net/?retryWrites=true&w=majority&appName=BookCR
+
+------------------------------------------------------------------------
+Creating the API references:
+
+- https://apidog.com/blog/how-to-create-a-rest-api-with-node-js-and-express/?utm_source=google_dsa&utm_medium=g&utm_campaign=20556541359&utm_content=160582184041&utm_term=&gad_source=1&gclid=CjwKCAiA0PuuBhBsEiwAS7fsNaXlLB9yPy99rdt5MsnZl-SQSbPU9EAdG2-lnk9hA_IkII59RSgjbBoCFUYQAvD_BwE
+
+- https://blog.postman.com/how-to-create-a-rest-api-with-node-js-and-express/
+
+- https://expressjs.com/en/guide/using-middleware.html
+
--- api/index.js
+++ api/index.js
@@ -4,7 +4,13 @@ import express from "express"
 //Se agrega libreria dotenv que sirve para cargar variables del archivo .env 
 import dotenv from "dotenv"
 //Se agrega libreria de mongoose para trabajar la base de datos. 
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+//Se importa el archivo de autenticacion para poder llamarla dentro de index. 
+import authRoute from "./routes/auth,js"
+
+//Aqui se declara la variable de app.
+const app = express()
+dotenv.config()
 
 //Se va a realizar la conexion de moongose tal como lo indica la pagina oficial de Mongoose
 const connect = async () => { //Esta es la funcion que se llamara cuando se conecte la app a la BD. 
@@ -26,10 +32,14 @@ mongoose.connection.on("connected", ()=>{
     console.log("mongoDB conectado")
 })
 
-const app = express()
-dotenv.config()
+//Aqui se va a crear endpoint para el API, se referencio la informacion de: https://apidog.com/blog/how-to-create-a-rest-api-with-node-js-and-express/?utm_source=google_dsa&utm_medium=g&utm_campaign=20556541359&utm_content=160582184041&utm_term=&gad_source=1&gclid=CjwKCAiA0PuuBhBsEiwAS7fsNaXlLB9yPy99rdt5MsnZl-SQSbPU9EAdG2-lnk9hA_IkII59RSgjbBoCFUYQAvD_BwE
+//Aqui se define la ruta del API que responde con un mensaje de tipo JSON por medio de un GET. 
+app.get("/home", (req,res)=>{
+  res.send("Hola, este es el primer request!")
+})
+
 //Vamos a crear la conexion con el puerto de la applicacion, se corre "npm start" en terminal o bash
 app.listen(8800,()=>{
     connect() //aqui se llama la funcion creada en la linea 10-16.
-    console.log("Conectado al backend")
+    console.log("Conectado al backend");
 })
