64251519200102a477f876e8bf0c235058a493c9
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 00:06:53 -0600

    Se mueven las operaciones CRUD aarchivos separados por orden, creando carpeta de controllers para las funciones que se usaran en las rutas

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,1 +1,14 @@
-export const createHotel = async (req, res, next)
+//se va a conectar con la BD para crear una coleccion de manera asincrona ya que no se hace inmediatamente. 
+export const createHotel = async (req, res, next) => {
+    //Aqui se obtiene la informacion del hotel del usuario.
+    const newHotel = new Hotel(req.body) //request es lo que se obtiene del usario.
+                                        //body es donde se va a almacenar la informacion del objeto. 
+
+    //Aqui vamos a manejar cualquier error al hacer la operacion  metodo create
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    }catch(err){
+        next(err); //aqui pasamos el error a express desde una funcion asyncrona. 
+    }
+}
