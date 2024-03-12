3106d4575daf1c74fb1a3d5e6ba8ba8c66e91d0e
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 01:22:31 -0600

    g

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -13,3 +13,18 @@ export const createHotel = async (req, res, next) =>{
         next(err); // Aqui se va a pasar el error a express para ser procesado.
     }
 }
+
+export const createHotel = async (req, res, next) =>{
+
+    //Aqui se obtiene la informacion del hotel del usuario.
+    const newHotel = new Hotel(req.body) //request es lo que se obtiene del usario.
+                                        //body es donde se va a almacenar la informacion del objeto. 
+
+    //Aqui vamos a manejar cualquier error al hacer la operacion  metodo create
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    }catch(err){
+        next(err); // Aqui se va a pasar el error a express para ser procesado.
+    }
+}
