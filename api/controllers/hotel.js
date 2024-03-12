2b91b3a4299f9d7bb61f2de49d0a679fa995fd36
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 02:07:01 -0600

    Se crearon los controllers para el crud de hotel, dejando las rutas libres con los endpoint.

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,3 +1,4 @@
+import Hotel from "../models/Hotel.js";
 // se va a conectar con la BD para crear una coleccion de manera asincrona ya que no se hace inmediatamente. 
 export const createHotel = async (req, res, next) =>{
 
@@ -22,6 +23,45 @@ export const updateHotel = async (req, res, next) =>{
         // indicando que se va actualizar el body, tambien se le pasa el parametro de "new" para que no haga un query cada vez que se actualizan los datos.
         const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body},{new:true})
         res.status(200).json(updatedHotel)
+    }catch(err){
+        next(err);
+    }
+}
+
+export const deleteHotel = async (req, res, next) =>{
+
+   //Aqui vamos a manejar cualquier error al hacer la operacion de metodo update
+   try{
+    //Aqui estamos buscando al hotel por el numero de ID, para luego borrarlo.
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted.")
+    }catch(err){
+    next(err);
+    }
+}
+
+export const getHotel = async (req, res, next) =>{
+
+     //Aqui vamos a manejar cualquier error al hacer la operacion de metodo update
+     try{
+        //Aqui estamos buscando un hotel en especifico por el numero de ID.
+    const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel)
+    }catch(err){
+        next(err);
+    }
+ }
+
+ export const getHotels = async (req, res, next) =>{
+
+    //const failed = true;
+    //if(failed) return next(err);
+
+    //Aqui vamos a manejar cualquier error al hacer la operacion de metodo update
+    try{
+        //Aqui estamos buscando todos los hoteles existentes.
+    const hotels = await Hotel.find();
+        res.status(200).json(hotels);
     }catch(err){
         next(err);
     }
