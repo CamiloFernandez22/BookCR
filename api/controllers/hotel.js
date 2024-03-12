b42480be26c87d3987b0186ae5b2b95cf1aa00a7
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 01:33:40 -0600

    Se mueve funcion de update a controllers

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -16,15 +16,13 @@ export const createHotel = async (req, res, next) =>{
 
 export const updateHotel = async (req, res, next) =>{
 
-    //Aqui se obtiene la informacion del hotel del usuario.
-    const newHotel = new Hotel(req.body) //request es lo que se obtiene del usario.
-                                        //body es donde se va a almacenar la informacion del objeto. 
-
-    //Aqui vamos a manejar cualquier error al hacer la operacion  metodo create
+    //Aqui vamos a manejar cualquier error al hacer la operacion de metodo update
     try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
+        //Aqui estamos buscando al hotel por el numero de ID, para luego actualizarlo con el metodo de Mongo SET,
+        // indicando que se va actualizar el body, tambien se le pasa el parametro de "new" para que no haga un query cada vez que se actualizan los datos.
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body},{new:true})
+        res.status(200).json(updatedHotel)
     }catch(err){
-        next(err); // Aqui se va a pasar el error a express para ser procesado.
+        next(err);
     }
 }
