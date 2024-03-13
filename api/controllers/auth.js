a4402b8bffdf1df1c274763f9304f147c29bbee9
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 23:23:21 -0600

    Se creo la funcion de annadir usuario en el archivo de controlador para usarse en la ruta

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,16 @@
 //Aqui se va a crear la funcion para el rigistro de usuarios 
+import User from "../models/User.js"
 export const register = async (req,res,next)=>{
     try{
-        const newUser = new User 
+        const newUser = new User({
+            username:req.body.username, //Se va a mandar la informacion de username a validar 
+            email:req.body.email, //Se va a mandar la informacion de email a validar 
+            password:req.body.password, //Se va a mandar la informacion de password a validar 
+        })
+
+        //Si la informacion es valida entonces vamos a implementar la siguiente funcion que devuelve status de creado
+        await newUser.save()
+        res.status(201).send("User created.")
     }catch(err){
         next(err)
     }
