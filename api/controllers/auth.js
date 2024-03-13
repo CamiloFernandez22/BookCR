0d30295f5d9c0ee604d85ce9a471d2ada3967fe3
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-13 02:24:46 -0600

    Se agrega el encriptado de contrasenna

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,12 +1,16 @@
 //Aqui se va a crear la funcion para el rigistro de usuarios 
 import User from "../models/User.js"
-
+import bcrypt from "bcryptjs"
 export const register = async (req,res,next)=>{
     try{
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username:req.body.username, //Se va a mandar la informacion de username a validar 
             email:req.body.email, //Se va a mandar la informacion de email a validar 
-            password:req.body.password, //Se va a mandar la informacion de password a validar 
+            password: hash, //Se va a mandar la informacion de password a validar 
         })
 
         //Si la informacion es valida entonces vamos a implementar la siguiente funcion que devuelve status de creado
