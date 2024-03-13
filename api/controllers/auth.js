5ee0b0686815e410a421e7fedde2d75a4dc96da9
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 23:01:51 -0600

    Se crea funcion para registrar usuarios en controlador

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,8 @@
 //Aqui se va a crear la funcion para el rigistro de usuarios 
 export const register = async (req,res,next)=>{
-
+    try{
+        const newUser = new User 
+    }catch(err){
+        next(err)
+    }
 }
