008437bec0c8f0b01c9cbc8f1e0d50b2755f2737
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-13 02:30:15 -0600

    Se agregan comentarios al controller de auth en relacion a bcryptjs

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,13 +4,14 @@ import bcrypt from "bcryptjs"
 export const register = async (req,res,next)=>{
     try{
 
+        //Se usa bcryptjs para encripitar la contrasenna en un hash
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
         const newUser = new User({
             username:req.body.username, //Se va a mandar la informacion de username a validar 
             email:req.body.email, //Se va a mandar la informacion de email a validar 
-            password: hash, //Se va a mandar la informacion de password a validar 
+            password: hash, //Se va a mandar la informacion de hash generado para la contrasenna a validar 
         })
 
         //Si la informacion es valida entonces vamos a implementar la siguiente funcion que devuelve status de creado
