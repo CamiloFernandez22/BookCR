305614aa3bb6cd43e3d2b58009173d7d4c029b1a
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-12 23:27:20 -0600

    Se modifico el archivo de ruta para auth, agregando la funcion de auth controlador

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 /*En esta ruta vamos a crear la autorizacion y autenticacion para asegurar que 
 solo los usuarios que tengan su token puedan acceder a su endpoint especifico*/
 
 //Aqui se va a instanciar la constante router que usa una funcion de express para aceptar los request del API
 const router = express.Router();
 
+//Aqui tenemos una ruta que va a indicar que se quiere crear un usuario
 router.get("/register", register)
 export default router
