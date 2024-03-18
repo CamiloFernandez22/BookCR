c7ce1d3be75eb6bcf2febc8712e35c0087b3f728
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-17 20:45:28 -0600

    Se actualiza la ruta de user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,32 @@
 import express from "express";
-/*En esta ruta vamos a crear los hoteles*/
+/*En esta ruta vamos a crear los users*/
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/User.js";
 
 //Aqui se va a instanciar la constante router que usa una funcion de express para aceptar los request del API
 const router = express.Router();
 
+//************************************************************************************************************************************ */
+//UPDATE se va a crear metodo de actualizar datos 
+
+router.put("/:id", updateUser);
+
+//************************************************************************************************************************************ */
+
+//DELETE
+
+router.delete("/:id", deleteUser);
+
+//************************************************************************************************************************************ */
+
+//GET
+
+router.get("/:id", getUser);
+
+//************************************************************************************************************************************ */
+
+//GET ALL
+
+router.get("/", getUsers);
 
 export default router
+
