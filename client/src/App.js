4164775de9440d9dee54465612af684407951d8e
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-19 23:12:58 -0600

    Se hicieron correcciones al codigo, comentarios dentro de child component ocasionaban error al interpretar en browser

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import{browserRouter, Routes, Route,} from "react-router-dom";
+import{BrowserRouter, Routes, Route,} from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
 function App() {
+  
   return (
-    <browserRouter>
     //Aqui vamos a crear las rutas para todas las paginas que se van a crear
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/> //Aqui se llama al componente de home 
+      <Route path="/hotels" element={<List/>}/> //Aqui se llama al componente de list el cual se visulizara cuando se acceda al url de hotels.
     </Routes>
-    </browserRouter>
+    </BrowserRouter>
   );
 }
 
