32b8f8d75be571c4c9d5cdd22936c9de19b1c9b9
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2024-03-27 23:02:11 -0600

    Se agregan iconos y titulos aheader para el eventual rediccionamiento de cada pagina/componente.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,39 @@
 import "./header.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBed, faCar, faDice, faPlaneDeparture} from "@fortawesome/free-solid-svg-icons"
+import { faPlane } from "@fortawesome/free-solid-svg-icons"
+
 const Header = () => {
     return (
+        //Se agregan los elementos visuales del header que van a direccionar al usuario a su respectiva pagina
         <div className="header">
-            Header
+            <div className="headerList">
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
+                </div>
+
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+                </div>
+
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car rentals</span>
+                </div>
+
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faDice} />
+                <span>Attractions</span>
+                </div>
+
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlaneDeparture} />
+                <span>Airport Taxi</span>
+                </div>
+
+            </div>
         </div>
     )
 } 
